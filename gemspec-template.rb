3886d93d293b8d130bfa523982107f829a6f5cb4
Jay Morlan <jay.d.morlan@gmail.com>
2016-04-25 15:06:17 -0500

    Create gemspec-template.rb

---
+++ gemspec-template.rb
@@ -0,0 +1,8 @@
+Gem::Specification.new do |spec|
+  # Required Values
+  spec.name = 'Your Gem Name'
+  spec.summary = 'What is your gem about?'
+  spec.version = '0.0.1'
+  spec.authors = ['jdmorlan']
+  spec.files = Dir['README.md', 'LICENSE.txt', 'lib/**/*']
+end
